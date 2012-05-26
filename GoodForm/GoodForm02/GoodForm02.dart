2c88c13fbe226feab2a5d5b5f26bd32fa1fbb62d
Gab B.-Estable <gabdude@gmail.com>
2012-05-26 19:25:30 -0400

    GoodForm02

---
+++ GoodForm/GoodForm02/GoodForm02.dart
@@ -0,0 +1,7 @@
+#import('dart:html');
+#source('Field.dart');
+#source('Cluster.dart');
+void main() {
+  // Get a reference to the canvas.
+  FormElement form = document.query('#ApplicationForm');
+}
